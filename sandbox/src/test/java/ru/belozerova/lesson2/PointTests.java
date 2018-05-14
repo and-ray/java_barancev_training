ea192d1fa9e9ce368181614b7589f0a1f4e8eb57
and-ray <redandray@yandex.ru>
2018-05-15 00:44:14 +0300

    Задание 3. Тесты к классу Point.

---
+++ sandbox/src/test/java/ru/belozerova/lesson2/PointTests.java
@@ -0,0 +1,39 @@
+package ru.belozerova.lesson2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+    public void testDistanceEqual(){
+        Point p1 = new Point(0,0);
+        // менее информативный метод ассерт.
+        assert p1.distanceTo(new Point(3,4))==5;
+
+    }
+
+    @Test
+    public void testDistanceEqual2(){
+        Point p1 = new Point(0,0);
+        Assert.assertEquals(p1.distanceTo(new Point(-6,-8)),10.0);
+
+    }
+
+    @Test
+    public void testDistanceNonEqual(){
+        Point p1 = new Point(0,0);
+        Assert.assertNotEquals(p1.distanceTo(new Point(-6,-8)),15.0);
+
+    }
+    @Test
+    public void testDistanceTrue(){
+        Point p1 = new Point(0,0);
+        Assert.assertTrue(p1.distanceTo(new Point(-6,-8))==10.0);
+    }
+    @Test
+    public void testDistanceNotNull(){
+        Point p1 = new Point(0,0);
+        Assert.assertNotNull(p1); //объект с нулевыми координатами, но не Null.
+    }
+}
