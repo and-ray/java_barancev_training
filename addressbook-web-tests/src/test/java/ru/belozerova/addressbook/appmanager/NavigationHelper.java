a76c0d09c47f380e0b05bf4e7fdc518b174a7289
and-ray <redandray@yandex.ru>
2018-05-20 22:57:11 +0300

    Задание 5, ч.4. Создание и внедрение суперкласса-хелпера HelperBase

--- addressbook-web-tests/src/test/java/ru/belozerova/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/belozerova/addressbook/appmanager/NavigationHelper.java
@@ -3,19 +3,18 @@ package ru.belozerova.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper {
-    private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(FirefoxDriver wd) {
-        this.wd=wd;
+        super(wd);
     }
 
     public void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 
     public void gotoHomePage() {
         //wd.get("http://localhost/addressbook/index.php");
-        wd.findElement(By.xpath("//a[text()='home']")).click();
+        click(By.xpath("//a[text()='home']"));
     }
 }
