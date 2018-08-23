db4c650449199becbf02ade899d51205a2120e98
and-ray <redandray@yandex.ru>
2018-08-23 23:43:14 +0300

    verifyContactListInUI

--- addressbook-web-tests/src/test/java/ru/belozerova/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/belozerova/addressbook/TestBase.java
@@ -4,6 +4,15 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.belozerova.addressbook.appmanager.ApplicationManager;
+import ru.belozerova.addressbook.model.ContactData;
+import ru.belozerova.addressbook.model.Contacts;
+import ru.belozerova.addressbook.model.GroupData;
+import ru.belozerova.addressbook.model.Groups;
+
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -18,6 +27,28 @@ public class TestBase {
     @AfterSuite
     public void tearDown() {
         app.stop();
+
     }
 
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream()
+                    .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
+    }
+    public void verifyContactListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Contacts dbContacts = app.db().contacts();
+            System.out.println("db" + dbContacts);
+            Contacts uiContacts = app.contact().all();
+            System.out.println("ui" + uiContacts);
+            assertThat(uiContacts, equalTo(dbContacts.stream()
+                    .map((g) -> new ContactData().withId(g.getId()).withFirstName(g.getFirstName())
+                    .withLastName(g.getLastName()))
+                    .collect(Collectors.toSet())));
+        }
+    }
 }
--- addressbook-web-tests/src/test/java/ru/belozerova/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/belozerova/addressbook/tests/ContactCreationTests.java
@@ -51,6 +51,7 @@ public class ContactCreationTests extends TestBase {
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.db().contacts();
         assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt()))));
+        verifyContactListInUI();
     }
 
     @Test (enabled = false)
