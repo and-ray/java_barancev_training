c8b68c70352c217fb60cc46cab2c7a9ebb2a4239
and-ray <redandray@yandex.ru>
2018-09-09 16:35:46 +0300

    изменение для дженкинса

--- sandbox/src/test/java/ru/belozerova/lesson2/PointTests.java
+++ sandbox/src/test/java/ru/belozerova/lesson2/PointTests.java
@@ -10,30 +10,37 @@ public class PointTests {
         Point p1 = new Point(0,0);
         // менее информативный метод ассерт.
         assert p1.distanceTo(new Point(3,4))==5;
-
     }
 
     @Test
     public void testDistanceEqual2(){
         Point p1 = new Point(0,0);
         Assert.assertEquals(p1.distanceTo(new Point(-6,-8)),10.0);
-
     }
 
     @Test
     public void testDistanceNonEqual(){
         Point p1 = new Point(0,0);
         Assert.assertNotEquals(p1.distanceTo(new Point(-6,-8)),15.0);
-
     }
+
     @Test
     public void testDistanceTrue(){
         Point p1 = new Point(0,0);
         Assert.assertTrue(p1.distanceTo(new Point(-6,-8))==10.0);
     }
+
     @Test
     public void testDistanceNotNull(){
         Point p1 = new Point(0,0);
         Assert.assertNotNull(p1); //объект с нулевыми координатами, но не Null.
     }
+
+    @Test
+    public void testDistanceEqual3(){
+        Point p1 = new Point(0,0);
+        // менее информативный метод ассерт.
+        assert p1.distanceTo(new Point(6,8))==10;
+    }
+
 }
